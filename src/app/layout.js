616c5cd17ad5acfc0676882cdd9b35d703a134a9
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-06-08 18:11:05 +0530

    basic setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -19,11 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={roboto.className}>
+        <main className="max-w-4xl mx-auto border-t p-4">
+          {children}
+        </main>
+        </body>
     </html>
   );
 }
---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+    <section>
+        <h1>Everything is better with a PIZZA</h1>
+        <p>Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life</p>
+        <Image src={} alt={} />
+    </section>
+  )
+}
+
+export default Hero
