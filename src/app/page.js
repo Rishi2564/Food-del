524d39795a2f36d88f5f186ecc8f14439bcb746c
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-06-09 17:06:46 +0530

    added section header

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,50 @@
 import Header from "@/components/layout/Header.jsx";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
-   <Header />
-   <Hero />
-   <HomeMenu />
+      <Header />
+      <Hero />
+      <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex-col gap-4">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+            nam voluptas autem repudiandae fugit in ipsam ab modi ducimus iste,
+            voluptatibus voluptatum, distinctio architecto voluptatem porro qui
+            et quos. Porro.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+            nam voluptas autem repudiandae fugit in ipsam ab modi ducimus iste,
+            voluptatibus voluptatum, distinctio architecto voluptatem porro qui
+            et quos. Porro.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+            nam voluptas autem repudiandae fugit in ipsam.
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Get in touch"}
+        />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500" href="tel:+919600289216">
+            +91 9600 289 216
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2025 All rights reserved
+      </footer>
     </>
   );
 }
