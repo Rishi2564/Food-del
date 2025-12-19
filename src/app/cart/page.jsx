b0ae41d409f013e6bf596b021c54516d8df3da15
Rishi2564 <rishabhpradhan2005@gmail.com>
2025-12-20 00:18:52 +0530

    Feat: added payment integration

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -5,30 +5,59 @@ import SectionHeader from "@/components/layout/SectionHeaders";
 import { Trash } from "lucide-react";
 import Image from "next/image";
 import React, { useContext, useEffect, useState } from "react";
-import {useProfile} from "@/components/UseProfile"
+import { useProfile } from "@/components/UseProfile";
+import toast from "react-hot-toast";
 const CartPage = () => {
   const { cartProducts, removeCartProduct } = useContext(CartContext);
-  const [address, setAddress] =useState({});
-  const {data:profileData}= useProfile();
-  useEffect(()=>{
-    if(profileData?.city){
-        const {phone, streetAddress, city, postalCode, country}= profileData;
-        const addressFromProfile={
-            phone,
-            streetAddress,
-            city,
-            postalCode,
-            country,
-        };
-        setAddress(addressFromProfile);
+  const [address, setAddress] = useState({});
+  const { data: profileData } = useProfile();
+  useEffect(() => {
+    if (profileData?.city) {
+      const { phone, streetAddress, city, postalCode, country } = profileData;
+      const addressFromProfile = {
+        phone,
+        streetAddress,
+        city,
+        postalCode,
+        country,
+      };
+      setAddress(addressFromProfile);
     }
-  },[profileData]);
-  let total = 0;
+  }, [profileData]);
+  let subtotal = 0;
   for (const p of cartProducts) {
-    total += cartProductPrice(p);
+    subtotal += cartProductPrice(p);
   }
-  function handleAddressChange(propName, value){
-    setAddress(prevAddress=>({...prevAddress,[propName]:value}))
+  function handleAddressChange(propName, value) {
+    setAddress((prevAddress) => ({ ...prevAddress, [propName]: value }));
+  }
+  async function proceedToCheckout(ev) {
+    ev.preventDefault();
+    const promise = new Promise((resolve, reject) => {
+      fetch("/api/checkout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          address,
+          cartProducts,
+        }),
+      }).then(async (response) => {
+        if(response.ok){
+          resolve();
+
+        window.location = await response.json();
+        } else {
+          reject();
+        }
+      });
+    });
+    await toast.promise(promise, {
+      loading: "Redirecting to checkout...",
+      success: "Redirecting to checkout",
+      error: "Error in checkout, please try again.",
+    }); 
   }
   return (
     <section className="mt-8">
@@ -83,18 +112,34 @@ const CartPage = () => {
               </div>
             ))}
           <div className="py-4 flex justify-between">
-            <span className="text-left pl-6 text-lg font-medium">Subtotal:</span>{" "}
-            <span className="text-lg font-semibold pr-20 text-right"> &#8377;{total}</span>{" "}
+            <span className="text-left pl-6 text-lg font-medium">
+              Subtotal:
+            </span>{" "}
+            <span className="text-lg font-semibold pr-20 text-right">
+              {" "}
+              &#8377;{subtotal}
+            </span>{" "}
+          </div>
+          <div className="py-4 flex justify-between">
+            <span className="text-left pl-6 text-lg font-medium">
+              Delivery:
+            </span>{" "}
+            <span className="text-lg font-semibold pr-20 text-right">
+              {" "}
+              &#8377;85
+            </span>{" "}
           </div>
         </div>
         <div className="bg-gray-100 p-4 rounded-2xl">
-            <h2>Checkout</h2>
-            <form action="">
-                <label htmlFor="">Address</label>
-                <AddressInput addressProps={address}
-                setAddressProps={handleAddressChange}  />
-                <button type="submit">Pay  &#8377;{total}</button>
-            </form>
+          <h2>Checkout</h2>
+          <form onSubmit={proceedToCheckout}>
+            <label htmlFor="">Address</label>
+            <AddressInput
+              addressProps={address}
+              setAddressProps={handleAddressChange}
+            />
+            <button type="submit">Pay &#8377;{subtotal + 85}</button>
+          </form>
         </div>
       </div>
     </section>
